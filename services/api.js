e3eb433f810864ac571fe445e83262617499192c
Willer Santos <64702678+Dev-Willer@users.noreply.github.com>
2024-09-29 14:09:21 -0300

    Update api.js

--- services/api.js
+++ services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://api-cadastro-de-usuarios-nine.vercel.app/" //http://localhost:3000
+    baseURL: "http://localhost:3000" //
 
 })
 
